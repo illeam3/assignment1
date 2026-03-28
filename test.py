4b74c622928680e662cc1079b8270a0e9c6d264c
illeam3 <miracles3431@gmail.com>
2026-03-28 21:34:40 +0900

    add basic test script setup

--- test.py
+++ test.py
@@ -15,3 +15,6 @@ def set_seed(seed=42):
     torch.manual_seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device:", device)
